7f61a30f82d5c9a20cd7f26d14d6b7a1b9f887cc
Adam Patterson <adam@adamrt.com>
2023-11-17 10:18:28 -0800

    Add minimal Vec2 and Vec3 classes

---
+++ src/Vector.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <cmath>
+
+struct Vec2 {
+    float x, y;
+    Vec2()
+        : x(0.0f)
+        , y(0.0f)
+    {
+    }
+    Vec2(float x, float y)
+        : x(x)
+        , y(y)
+    {
+    }
+};
+
+struct Vec3 {
+    float x, y, z;
+
+    Vec3()
+        : x(0.0f)
+        , y(0.0f)
+        , z(0.0f)
+    {
+    }
+
+    Vec3(float x, float y, float z)
+        : x(x)
+        , y(y)
+        , z(z)
+    {
+    }
+
+    Vec3 rotate_x(float angle) const
+    {
+        float c = std::cos(angle);
+        float s = std::sin(angle);
+        return Vec3(x, y * c - z * s, y * s + z * c);
+    }
+
+    Vec3 rotate_y(float angle) const
+    {
+        float c = std::cos(angle);
+        float s = std::sin(angle);
+        return Vec3(x * c + z * s, y, -x * s + z * c);
+    }
+
+    Vec3 rotate_z(float angle) const
+    {
+        float c = std::cos(angle);
+        float s = std::sin(angle);
+        return Vec3(x * c - y * s, x * s + y * c, z);
+    }
+};
