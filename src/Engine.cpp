8d20922190970a5b6ab21ec2187cef893ffe34f1
Adam Patterson <adam@adamrt.com>
2023-11-17 14:31:34 -0800

    Draw meshes of faces/vertices

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -1,13 +1,42 @@
-#include "Engine.h"
 #include "SDL_timer.h"
-#include "Vector.h"
-#include "Window.h"
 #include <iostream>
 
-std::vector<Vec3> cube_points;
-std::vector<Vec2> projected_points;
+#include "Engine.h"
+#include "Mesh.h"
+#include "Triangle.h"
+#include "Vector.h"
+#include "Window.h"
+
 Vec3 cube_rotation {};
 float fov_factor = 640.0f;
+
+std::vector<Triangle> triangles_to_render {};
+
+Mesh mesh(
+    {
+        { -1, -1, -1 }, // 1
+        { -1, 1, -1 },  // 2
+        { 1, 1, -1 },   // 3
+        { 1, -1, -1 },  // 4
+        { 1, 1, 1 },    // 5
+        { 1, -1, 1 },   // 6
+        { -1, 1, 1 },   // 7
+        { -1, -1, 1 }   // 8
+    },
+    {
+        { 1, 2, 3, 0xFF0000FF }, // front
+        { 1, 3, 4, 0x00FF00FF },
+        { 4, 3, 5, 0x0000FFFF }, // right
+        { 4, 5, 6, 0xFFFF00FF },
+        { 6, 5, 7, 0x00FFFFFF }, // back
+        { 6, 7, 8, 0xA500FFFF },
+        { 8, 7, 2, 0xFFFFFFFF }, // left
+        { 8, 2, 1, 0x990000FF },
+        { 2, 7, 5, 0x009900FF }, // top
+        { 2, 5, 3, 0x000099FF },
+        { 6, 8, 1, 0x999900FF }, // bottom
+        { 6, 1, 4, 0x009999FF },
+    });
 
 Engine::Engine(Framebuffer& fb, Window& window, UI& ui)
     : m_framebuffer(fb)
@@ -18,13 +47,6 @@ Engine::Engine(Framebuffer& fb, Window& window, UI& ui)
 
 void Engine::setup()
 {
-    for (float x = -1; x < 1; x += 0.25) {
-        for (float y = -1; y < 1; y += 0.25) {
-            for (float z = -1; z < 1; z += 0.25) {
-                cube_points.push_back(Vec3(x, y, z));
-            }
-        }
-    }
 }
 
 void Engine::process_input()
@@ -48,6 +70,7 @@ void Engine::process_input()
 
 void Engine::update()
 {
+    triangles_to_render.clear();
 
     int delay = FRAME_TARGET_TIME - (SDL_GetTicks() - m_previous_frame_time);
 
@@ -61,36 +84,65 @@ void Engine::update()
         return Vec2(p.x / p.z * fov_factor, p.y / p.z * fov_factor);
     };
 
-    cube_rotation.x += 0.02f;
-    cube_rotation.y += 0.012f;
-    cube_rotation.z += 0.018f;
+    if (m_ui.rotate) {
+        cube_rotation.x += 0.008f;
+        cube_rotation.y += 0.008f;
+        cube_rotation.z += 0.008f;
+    }
 
-    projected_points.clear();
-    for (auto& p : cube_points) {
+    auto w = m_framebuffer.width();
+    auto h = m_framebuffer.height();
 
-        auto transformed = p.rotate_x(cube_rotation.x);
-        transformed = transformed.rotate_y(cube_rotation.y);
-        transformed = transformed.rotate_z(cube_rotation.z);
+    for (auto& face : mesh.faces) {
+        auto face_vertices = std::vector<Vec3> {
+            mesh.vertices[face.a - 1],
+            mesh.vertices[face.b - 1],
+            mesh.vertices[face.c - 1]
+        };
 
-        // Push points away from the camera.
-        transformed.z += 5.0f;
+        Triangle projected_triangle {};
+        projected_triangle.color = face.color;
 
-        projected_points.push_back(project(transformed));
+        for (auto transformed : face_vertices) {
+            transformed = transformed.rotate_x(cube_rotation.x);
+            transformed = transformed.rotate_y(cube_rotation.y);
+            transformed = transformed.rotate_z(cube_rotation.z);
+
+            // Push points away from the camera.
+            transformed.z += 5.0f;
+
+            Vec2 projected = project(transformed);
+            projected.x += (w / 2.0f);
+            projected.y += (h / 2.0f);
+
+            projected_triangle.points.push_back(projected);
+        }
+
+        triangles_to_render.push_back(projected_triangle);
     }
 }
 
 void Engine::render()
 {
-    auto w = m_framebuffer.width();
-    auto h = m_framebuffer.height();
-
     m_framebuffer.clear(0x333333FF);
 
-    for (auto& p : projected_points) {
-        auto centered_x = p.x + (w / 2.0f);
-        auto centered_y = p.y + (h / 2.0f);
-
-        m_framebuffer.draw_rect(centered_x, centered_y, 2, 2, 0xFFFF00FF);
+    if (m_ui.filled) {
+        for (auto& t : triangles_to_render) {
+            m_framebuffer.draw_triangle_filled(
+                t.points[0].x, t.points[0].y,
+                t.points[1].x, t.points[1].y,
+                t.points[2].x, t.points[2].y,
+                t.color);
+        }
+    } else {
+        for (auto& t : triangles_to_render) {
+            m_framebuffer.draw_triangle(
+                t.points[0].x, t.points[0].y,
+                t.points[1].x, t.points[1].y,
+                t.points[2].x, t.points[2].y,
+                0xA500FFFF);
+        }
     }
+
     m_window.render();
 }
