24e1fb36ddaf7bd368e2d38238954480c0a17c41
Adam Patterson <adam@adamrt.com>
2023-11-21 14:13:03 -0800

    Add kclamp and kradians

--- src/AK.h
+++ src/AK.h
@@ -1,4 +1,7 @@
 // This is a header file for Adam's Kit.
+#pragma once
+#include <algorithm>
+#include <cmath>
 #include <cstdint>
 
 using i8 = int8_t;
@@ -13,3 +16,16 @@ using u64 = uint64_t;
 
 using f32 = float;
 using f64 = double;
+
+constexpr f32 K_PI = 3.141592653589793;
+constexpr f32 DEG2RAD_MUL(K_PI / 180.0f);
+
+inline f32 kclamp(f32 n, f32 lower, f32 upper)
+{
+    return std::max(lower, std::min(n, upper));
+}
+
+inline f32 kradians(f32 degrees)
+{
+    return degrees * DEG2RAD_MUL;
+}
