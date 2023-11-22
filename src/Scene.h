b39ac3cc5801853e76b211f10cf86301f7680d83
Adam Patterson <adam@adamrt.com>
2023-11-22 07:37:10 -0800

    Add basic Scene and Model classes

---
+++ src/Scene.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <vector>
+
+#include "Light.h"
+#include "Mesh.h"
+#include "Texture.h"
+#include "Triangle.h"
+
+class Model {
+public:
+    Model(const Mesh& mesh, const Texture& texture)
+        : mesh(mesh)
+        , texture(texture)
+    {
+    }
+
+    const Mesh& mesh;
+    const Texture& texture;
+
+    Vec3 scale = Vec3(1.0f, 1.0f, 1.0f);
+    Vec3 rotation = Vec3(0.0f, 0.0f, 0.0f);
+    Vec3 translation = Vec3(0.0f, 0.0f, 0.0f);
+
+    std::vector<Triangle> to_render;
+};
+
+struct Scene {
+    std::vector<Model> models;
+    std::vector<PointLight> lights;
+    AmbientLight ambient_light;
+};
